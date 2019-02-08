bb4f3101fa343ddd2c33d480cd519688a6cb0d8f
Bananek96 <Banasiak_Jakub@wp.pl>
2019-02-08 08:45:29 +0100

    elo

--- uczniowie/modele.py
+++ uczniowie/modele.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#  modele.py
 
 from peewee import *
 
-baza_plik = 'quiz.db'
-baza = SqliteDatabase(baza_plik)  # instancja bazy
+baza_plik = 'uczniowie.db'
+baza = SqliteDatabase(baza_plik) #instalacja bazy
 
-### MODELE #
+#### MODELE DANYCH
 class BazaModel(Model):
     class Meta:
         database = baza
 
-
 class Klasa(BazaModel):
-    pass
-
-
+    
+    klasa = CharField(null=False)
+    roknaboru = IntegerField(default=0)
+    rokmatury = IntegerField(default=0)
+    
 class Uczen(BazaModel):
-    pass
+    
+    imie = CharField(null=False)
+    nazwisko = CharField(null=False)
+    plec = IntegerField()
+    klasa = ForeignKeyField(Klasa, related_name='uczniowie')
+        
+class Przedmiot(BazaModel):
+    
+    przedmiot = CharField(null=False)
+    imie_naucz = CharField(null=False)
+    nazwisko_naucz = CharField(null=False)
+    plec_naucz = IntegerField()
+    
+class Ocena(BazaModel):
+    
+    data_d = DateTimeField()
+    uczen = ForeignKeyField(Uczen, related_name='oceny')
+    przedmiot = ForeignKeyField(Przedmiot, related_name='oceny')
+    ocena = DecimalField(null=False)
+    
 
-
-def main(args):
-    # Uwaga: po utworzeniu modeli uruchom plik modele.py
-    # jeden raz w środowisku z zainstalowaną biblioteką peewee:
-    # python modele.py
-    baza.connect()
-    baza.create_tables([Klasa, Uczen])
-
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
